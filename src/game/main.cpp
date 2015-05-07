9e6acda6540a9b40f366754044b4a3821618393e
Anton Ingfors <Anton.Ingfors@gmail.com>
2015-05-07 22:03:05 +0200

    Change syntax to better suit our coding conversion

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -5,8 +5,8 @@
 
 int main( int args, char *argv[])
 {
-	game::cmain_state main_state;
-	state::cstack stack;
-	stack.run( main_state );
+	Game::CMainState main_state;
+	State::CStack stack;
+	stack.Run( main_state );
 	return 0;
 }
