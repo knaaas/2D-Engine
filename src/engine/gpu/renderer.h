58a911aee887ed714698213570da3ffda5be1f69
Wddy <andre.s.lundberg@gmail.com>
2015-05-08 16:46:47 +0200

    Update renderer.h

--- src/engine/gpu/renderer.h
+++ src/engine/gpu/renderer.h
@@ -6,16 +6,38 @@
 
 namespace Engine
 {
+	/*
+		There are a possibility that this file will be moved to /scene. 
+		Don't relie on this file's path or class-name, typedef them. 
+		
+		The functionality is unlikely to change. 
+		
+		The Render function dosen't do anything except calling render in
+		instance, but garanties that the data will be of T-type. This 
+		maybe changed in the future. 
+		
+		The "Program" functions will removed in near future. Don't make use of  
+	*/
+	
 	template <typename T>
 	class CGPURenderer
 	{
 		public:
 			CGPURenderer();
 			
+			/*
+				Enable/Disable should be called before/after Render. 
+				Try to render everything of T-type between and don't
+				Enable/Disable to much. it will have a  performance 
+				impact. 
+			*/
+			
 			void Enable();
 			void Disable();
 			
 			void Render( CGPUInstance<T> * instance, EDrawMode mode, GLuint count );
+			
+			
 			
 			CProgram& Program();
 			const CProgram& Program() const;
