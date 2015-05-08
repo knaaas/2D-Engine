9cba72f787e9f13e1642795d53ea964510c5b5f1
Wddy <andre.s.lundberg@gmail.com>
2015-05-08 16:56:38 +0200

    Update instance.h

--- src/engine/scene/instance.h
+++ src/engine/scene/instance.h
@@ -6,11 +6,31 @@
 
 namespace Engine
 {
+	/*
+		This should only be created once,
+		due to my lazy-ass and some limitations in OpenGL.
+		
+		To solve the issue, different uniform-buffer indexes 
+		must be used in different instances. Or at least I 
+		believe so, so far as I know. 
+		
+		NOTE: 
+		Singleton is ugly, don't do it.  
+		
+	*/
 	class CSCEInstance
 	{
 		public:
 			CSCEInstance( CSCECamera *camera );
 			~CSCEInstance();
+			
+			/*
+				Make a renderer usable in this instance.
+				
+				Note: It can only belong to one instance,
+				calling the function in another instance
+				will break the old. 
+			*/
 			
 			template <typename T>
 			bool Initialize( CGPURenderer<T> *renderer );
